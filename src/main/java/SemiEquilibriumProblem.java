e7cb593ad30f366a9e423af73a570a6dcc3ffbad
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-06-20 20:04:58 +0200

    Add SemiEquilibrium solution and tests

--- src/main/java/SemiEquilibriumProblem.java
+++ src/main/java/SemiEquilibriumProblem.java
@@ -1,2 +1,25 @@
 public class SemiEquilibriumProblem {
+
+    public int semiIndex(int[] array) {
+        int sum = sum(array);
+        if(sum == 0)
+            return -1;
+        int halfSum = sum/2 + 1; // minimal value that needs to be reached to fulfill the condition
+        int i=0;
+        int partialSum = 0;
+        while(partialSum<halfSum) {
+            partialSum += array[i];
+            i++;
+        }
+        return i-1;
+    }
+
+
+    private int sum(int[] array) {
+        int tempSum = 0;
+        for(int i=0; i<array.length; i++) {
+            tempSum += array[i];
+        }
+        return tempSum;
+    }
 }
--- src/test/java/SemiEquilibriumProblemTest.java
+++ src/test/java/SemiEquilibriumProblemTest.java
@@ -1,2 +1,44 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SemiEquilibriumProblemTest {
+
+    SemiEquilibriumProblem semiEquilibrium = new SemiEquilibriumProblem();
+
+    @Test
+    public void setSemiEquilibriumTest1() {
+        int[] array = {1,2,5,2,1};
+        Assert.assertEquals(2, semiEquilibrium.semiIndex(array));
+    }
+
+    @Test
+    public void setSemiEquilibriumTest2() {
+        int[] array = {1};
+        Assert.assertEquals(0, semiEquilibrium.semiIndex(array));
+    }
+
+    @Test
+    public void setSemiEquilibriumTest3() {
+        int[] array = {1,1};
+        Assert.assertEquals(1, semiEquilibrium.semiIndex(array));
+    }
+
+    @Test
+    public void setSemiEquilibriumTest4() {
+        int[] array = {1,1,1};
+        Assert.assertEquals(1, semiEquilibrium.semiIndex(array));
+    }
+
+    @Test
+    public void setSemiEquilibriumTest5() {
+        int[] array = {0,0,0,1};
+        Assert.assertEquals(3, semiEquilibrium.semiIndex(array));
+    }
+
+    @Test
+    public void setSemiEquilibriumTest6() {
+        int[] array = {0,0,0,0};
+        Assert.assertEquals(-1, semiEquilibrium.semiIndex(array));
+    }
+
 }
